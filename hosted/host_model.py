211beeba4af75be4a6919432c41d6c1c09fca7d4
dgketchum <dgketchum@gmail.com>
2023-01-25 15:16:37 -0700

    deploy gs model

--- hosted/host_model.py
+++ hosted/host_model.py
@@ -12,15 +12,13 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 from tensorflow.python.tools import saved_model_utils
-from tensorflow.python.lib.io import file_io
 
 from call_ee import PROPS, stack_bands
-
-PROPS = ['elevation', 'slope', 'aspect', 'lat', 'lon']
+from assets import list_bucket_files
 
 home = os.path.expanduser('~')
 conda = os.path.join(home, 'miniconda3', 'envs')
@@ -38,17 +36,27 @@ DATA_BUCKET = 'wudr'
 OUTPUT_BUCKET = 'wudr'
 REGION = 'us-central1'
 MODEL_DIR = 'gs://wudr/ept_model'
+TRAINING_DATA = 'gs://wudr/tfr_29DEC2022'
 EEIFIED_DIR = 'gs://wudr/ept_model_eeified'
-VERSION_NAME = 'v01'
+VERSION_NAME = 'v00'
+
+PROPS = ['elevation', 'slope', 'aspect', 'lat', 'lon', 'etr_gs', 'ppt_wy_et', 'et_gs']
+
+
+# ET_COLS = ['et_{}'.format(mm) for mm in range(4, 11)]
+# PROPS = PROPS + ['et_gs']
 
 
 class DNN:
-    def __init__(self, label=None, _dir=None):
+    def __init__(self, month=None, label=None, _dir=None):
+        self.indices = None
         self.normalizer = None
         self.model_dir = _dir
         self.label = label
         self.model = None
         self.model_name = None
+        self.month = month
+        self.target = 'et_{}'.format(month)
 
         if _dir and label:
             self.ee_dir = os.path.join(EEIFIED_DIR, self.label)
@@ -57,72 +65,100 @@ class DNN:
             columns = [tf.io.FixedLenFeature(shape=[1], dtype=tf.float32) for _ in self.feature_names]
             self.features_dict = dict(zip(self.feature_names, columns))
 
-    def prepare(self, x):
-        self.normalizer = tf.keras.layers.Normalization(axis=-1)
-        self.normalizer.adapt(x)
-        self.model = self._build_and_compile_model()
-
-    def _build_and_compile_model(self):
-
+    def prepare(self, n_inputs):
         model = tf.keras.models.Sequential([
-            tf.keras.layers.Input((None, None, len(PROPS),)),
+            tf.keras.layers.Input((None, None, n_inputs,)),
             tf.keras.layers.Conv2D(64, (1, 1), activation=tf.nn.relu),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dropout(0.1),
-            tf.keras.layers.Conv2D(1, (1, 1), activation=tf.nn.softmax)
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Conv2D(1, (1, 1), activation='linear')
         ])
         model.compile(loss='mean_absolute_error',
-                      optimizer=tf.keras.optimizers.Adam(0.001))
-        return model
+                      optimizer=tf.keras.optimizers.Adam(0.01))
+        self.model = model
+
+    def parse_tfrecord(self, example_proto):
+        parsed_features = tf.io.parse_single_example(example_proto, self.features_dict)
+
+        if self.target == 'et_gs':
+            labels = parsed_features[self.target]
+        else:
+            labels = parsed_features.pop(self.target)
+
+        return parsed_features, tf.cast(labels, tf.float32)
 
     def to_tuple(self, inputs, label):
-        return (tf.expand_dims(tf.transpose(list(inputs.values())), 1),
-                tf.expand_dims(tf.transpose(list(label.values())), 1))
+        lb1 = tf.expand_dims(label, 1)
+        lb1 = tf.expand_dims(lb1, 1)
+        feats = tf.expand_dims(tf.transpose(list(inputs.values())), 1)
+        feats = tf.gather(feats, self.indices, axis=-1)
+        return feats, lb1
 
-    def _prep_csv(self, csv, month):
-        c = pd.read_csv(csv)
-        t_et_cols = ['et_{}'.format(mm) for mm in range(4, 11)]
-        for etc in t_et_cols:
-            c[etc] = c[etc].values.astype(float) * 0.00001
+    @tf.function
+    def _filtercond(self, x, _):
+        return tf.less(x['et_gs'], x['ppt_wy_et'])
 
-        return c
+    def train(self, batch):
 
+        # training_data = list_bucket_files(TRAINING_DATA)
+        training_data = [os.path.join('/media/nvm/tfr/', x) for x in os.listdir('/media/nvm/tfr/')]
+        dataset = tf.data.TFRecordDataset(training_data, compression_type='GZIP')
 
+        for raw_record in dataset.take(1):
+            example = tf.train.Example()
+            example.ParseFromString(raw_record.numpy())
+            print(example)
 
-    def train(self, csv, month):
-        c = self._prep_csv(csv, month)
+        split = 5
+        train_dataset = dataset.window(split, split + 1).flat_map(lambda ds: ds)
+        parsed_training = train_dataset.map(self.parse_tfrecord, num_parallel_calls=5)
+        filtered = parsed_training.filter(lambda x, y: tf.reduce_all(x['et_gs'] < x['ppt_wy_et']))
 
-        split = int(c.shape[0] * 0.8)
+        # remove 'et_gs' from training data
+        filt = iter(filtered).next()
+        keys = list(filt[0].keys())
+        ind = keys.index('et_gs')
+        self.indices = [x for x in range(len(keys)) if x != ind]
+        keys.pop(ind)
+        input_dataset = filtered.map(self.to_tuple)
+        input_dataset = input_dataset.shuffle(128).batch(batch)
 
-        targets, features, first = [], None, True
-        df = deepcopy(c.iloc[:split, :])
-        target = 'et_{}'.format(month)
-        targets.append(target)
+        test_dataset = dataset.skip(split).window(1, split + 1).flat_map(lambda ds: ds)
+        parsed_test = test_dataset.map(self.parse_tfrecord, num_parallel_calls=5)
+        filtered = parsed_test.filter(lambda x, y: tf.reduce_all(x['et_gs'] < x['ppt_wy_et']))
+        input_test = filtered.map(self.to_tuple)
+        input_test = input_test.batch(batch)
 
-        df.dropna(axis=0, inplace=True)
-        y = tf.convert_to_tensor(df[target].values.astype('float32'))
-        df = df[PROPS]
-        x = tf.convert_to_tensor(df.values.astype('float32'))
-        print('training on {}'.format(df.shape[0]))
+        self.prepare(len(keys))
 
-        val = deepcopy(c.iloc[split:, :])
-        val.dropna(axis=0, inplace=True)
-        y_test = tf.convert_to_tensor(val[target].values.astype('float32'))
-        val = val[PROPS]
-        x_test = tf.convert_to_tensor(val.values.astype('float32'))
-        print('validating on {}'.format(val.shape[0]))
+        reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.8, patience=3, min_lr=0.001)
+        early_stopping = EarlyStopping(monitor='val_loss', patience=7, mode='auto', restore_best_weights=True)
+        self.model.fit(x=input_dataset, verbose=1, epochs=100, validation_split=0.,
+                       validation_data=input_test, validation_freq=1, callbacks=[reduce_lr, early_stopping])
 
-        self.prepare(x)
-        self.model.fit(x, y, verbose=1, epochs=20, batch_size=2560)
-        y_pred = self.model.predict(x_test).flatten()
-
+        y_pred = self.model.predict(x=input_test)[:, 0, 0, :]
+        x_test = np.concatenate([x for x, y in input_test], axis=0)[:, 0, 0, :]
+        y_test = np.concatenate([y for x, y in input_test], axis=0)[:, 0, 0, :]
+        df = pd.DataFrame(columns=['y_test', 'y_pred'], data=np.array([y_test, y_pred])[:, :, 0].T)
+        df[keys] = x_test
         rmse = mean_squared_error(y_test, y_pred, squared=False)
-
-        mstr = str(m)
-        print('\n month {}'.format(mstr))
-        print('observed ET: {:.3f} m'.format(np.array(y).mean()))
-        print('rmse ET: {:.3f} mm'.format(rmse * 1000))
-        print('rmse {:.3f} %'.format(rmse / np.array(y).mean() * 100.))
-        print('trained on {}'.format(PROPS))
+        print('target {}'.format(self.target))
+        print('observed ET: {:.3f} mm'.format(y_test.mean()))
+        print('predicted ET: {:.3f} mm'.format(y_pred.mean()))
+        print('rmse ET: {:.3f} mm'.format(rmse))
+        print('rmse {:.3f} %'.format(rmse / np.array(y_test).mean() * 100.))
+        print('trained on {}\n'.format(keys))
 
     def save(self):
         self.model.save(self.model_dir, save_format='tf')
@@ -242,27 +278,6 @@ class DNN:
     def load(self):
         self.model = tf.keras.models.load_model(self.model_dir)
 
-    def predict_csv(self, csv, month):
-
-        if not self.model:
-            raise NotImplemented
-
-        df = self._prep_csv(csv, month)
-        target = 'et_{}'.format(month)
-
-        df.dropna(axis=0, inplace=True)
-        y = tf.convert_to_tensor(df[target].values.astype('float32'))
-        df = df[PROPS]
-        x = tf.convert_to_tensor(df.values.astype('float32'))
-        y_pred = self.model.predict(x).flatten()
-        rmse = mean_squared_error(y, y_pred, squared=False)
-
-        print('\n month {}'.format(m))
-        print('observed ET: {:.3f} m'.format(np.array(y).mean()))
-        print('rmse ET: {:.3f} mm'.format(rmse * 1000))
-        print('rmse {:.3f} %'.format(rmse / np.array(y).mean() * 100.))
-        print('predicted on {}'.format(PROPS))
-
 
 if __name__ == '__main__':
     root = '/media/research/IrrigationGIS'
@@ -275,17 +290,15 @@ if __name__ == '__main__':
     clip = 'users/dgketchum/expansion/columbia_basin'
     asset_rt = ic = 'users/dgketchum/expansion/eff_ppt_gcloud'
 
-    for m in range(4, 5):
-        t = 'et_{}'.format(m)
-        model_dir = os.path.join(MODEL_DIR, t)
-        nn = DNN(label=t, _dir=model_dir)
-        nn.model_name = MODEL_NAME.format(m)
-        nn.train(training, m)
-        # nn.save()
-        # nn.load(model_dir)
-        # nn.predict_csv(training, m)
-        # nn.deploy()
-        # scale_ = 1000
-        # nn.infer(asset_rt, clip, scale_, 2020, m)
+    m = 'gs'
+    t = 'et_{}'.format(m)
+    model_dir = os.path.join(MODEL_DIR, t)
+    nn = DNN(month=m, label=t, _dir=model_dir)
+    nn.model_name = MODEL_NAME.format(m)
+    nn.train(256)
+    nn.save()
+    nn.deploy()
+    # scale_ = 1000
+    # nn.infer(asset_rt, clip, scale_, 2020, m)
 
 # ========================= EOF ====================================================================
