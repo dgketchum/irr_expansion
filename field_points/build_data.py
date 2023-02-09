85cefb6322eca371f1ebbc7ab3ae24527cb5db14
dgketchum <dgketchum@gmail.com>
2023-02-09 12:04:04 -0700

    calculate field-wise spei and simi

--- field_points/build_data.py
+++ field_points/build_data.py
@@ -1,28 +1,40 @@
 import os
-import json
 
 import numpy as np
 import pandas as pd
-import pandas_tfrecords as pdtfr
-import geopandas as gpd
 
 from call_ee import BASIN_STATES
+from climate_indices import compute, indices
 
 COLS = ['et', 'cc', 'ppt', 'etr', 'eff_ppt', 'ietr']
 META_COLS = ['STUSPS', 'x', 'y', 'name', 'usbrid']
 
+IDX_KWARGS = dict(distribution=indices.Distribution.gamma,
+                  data_start_year=1987,
+                  calibration_year_initial=1987,
+                  calibration_year_final=2009,
+                  periodicity=compute.Periodicity.monthly)
 
-def concat_tfr(tfr_dir, metadata, out):
+
+def concat_tfr(tfr_dir, metadata, out, subset='all'):
+    import pandas_tfrecords as pdtfr
 
     for s in BASIN_STATES:
-        if s in ['AZ', 'CA']:
+        if s != 'CA':
             continue
         print('\n', s)
-        mdata = os.path.join(metadata, 'points_metadata_{}.csv'.format(s))
+        mdata = os.path.join(metadata, '{}.csv'.format(s))
         df = pd.read_csv(mdata, index_col='OPENET_ID')
         df = df[~df.index.duplicated()]
 
-        dt_range = ['{}-{}-01'.format(y, m) for y in range(1987, 1990) for m in range(1, 13)]
+        if subset == 'usbr':
+            df = df[df['usbrid'] > 0]
+        elif subset == 'nonusbr':
+            df = df[df['usbrid'] == 0]
+        else:
+            pass
+
+        dt_range = ['{}-{}-01'.format(y, m) for y in range(1987, 2010) for m in range(1, 13)]
         dt_ind = pd.DatetimeIndex(dt_range)
         midx = pd.MultiIndex.from_product([df.index, dt_ind], names=['idx', 'dt'])
         mdf = pd.DataFrame(index=midx, columns=COLS)
@@ -39,14 +51,23 @@ def concat_tfr(tfr_dir, metadata, out):
             if not 3 < m < 11:
                 c[['et', 'cc', 'eff_ppt', 'ietr']] = np.zeros((c.shape[0], 4))
             c = c[COLS]
-            mdf.loc[(c.index, dt), c.columns] = c.values
+            match_idx = [i for i in c.index if i in mdf.index]
+            mdf.loc[(match_idx, dt), c.columns] = c.loc[match_idx].values
 
-        os.path.join(out, '{}.csv'.format(s))
-        mdf.to_csv()
+        out_path = os.path.join(out, '{}.csv'.format(s))
+        mdf.to_csv(out_path)
 
 
 def map_indices(csv, out_csv):
     df = pd.read_csv(csv, index_col=['idx', 'dt'])
+    df['kc'] = df['et'] / df['ietr']
+    simi = df.groupby(level=0).apply(lambda x: indices.spi(x.kc.values, scale=5, **IDX_KWARGS))
+    simi = np.stack(simi.values).flatten()
+    spei = df.groupby(level=0).apply(lambda x: indices.spei(x.ppt.values, x.etr.values, scale=11, **IDX_KWARGS))
+    spei = np.stack(spei.values).flatten()
+    resp_data = np.array([simi, spei]).T
+    compose = pd.DataFrame(data=resp_data, columns=['simi', 'spei'], index=df.index)
+    example = compose.loc['CA_281937']
     pass
 
 
@@ -58,7 +79,7 @@ if __name__ == '__main__':
     tfr_ = '/media/nvm/field_pts/tfr'
     out_ = '/media/nvm/field_pts/field_pts_data'
     meta_ = '/media/nvm/field_pts/metadata/'
-    # concat_tfr(tfr_, meta_, out_)
+    concat_tfr(tfr_, meta_, out_, subset='all')
 
     in_ = '/media/nvm/field_pts/field_pts_data/CA.csv'
     out_ = '/media/nvm/field_pts/indices/CA.csv'
