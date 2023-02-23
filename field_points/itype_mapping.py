b5497f84923e174d0207e325dc234233c178e687
dgketchum <dgketchum@gmail.com>
2023-02-22 22:34:37 -0700

    initial commit, itype_mapping.py

---
+++ field_points/itype_mapping.py
@@ -0,0 +1,55 @@
+import os
+
+
+def itype():
+    map_ = {'Rill/Sprinkler/Wheel Line': None,
+            'Center Pivot/Drip': None,
+            'Drip/None': None,
+            'Flood': None,
+            'Rill/Wheel Line': None,
+            'Rill/Sprinkler': None,
+            'Drip/Rill': None,
+            'Center Pivot/Rill/Wheel Line': None,
+            'Rill': None,
+            'Big Gun/Drip': None,
+            'Center Pivot': None,
+            'Big Gun/Center Pivot': None,
+            'Drip/Rill/Sprinkler': None,
+            'Drip/Sprinkler': None,
+            'Wheel Line': None,
+            'Center Pivot/Rill': None,
+            'Micro-Sprinkler': None,
+            'Hand': None,
+            'Big Gun': None,
+            'Drip': None,
+            'Big Gun/Wheel Line': None,
+            'Big Gun/Sprinkler': None,
+            'Drip/Big Gun': None,
+            'Drip/Micro-Sprinkler': None,
+            'Sprinkler/Wheel Line': None,
+            'Center Pivot/Sprinkler': None,
+            'Drip/Sprinkler/Wheel Line': None,
+            'Center Pivot/None': None,
+            'Sprinkler': None,
+            'Center Pivot/Sprinkler/Wheel Line': None,
+            'Hand/Sprinkler': None,
+            'Center Pivot/Rill/Sprinkler': None,
+            'Drip/Wheel Line': None,
+            'Center Pivot/Wheel Line': None,
+            'Micro Sprinkler': None,
+            'GRATED_PIPE': None,
+            'DRIP': None,
+            'UNKNOWN': None,
+            'SPRINKLER': None,
+            'FURROW': None,
+            'FLOOD': None,
+            'P': None,
+            'D': None,
+            'S': None,
+            'F': None}
+    return map_
+
+
+if __name__ == '__main__':
+    pass
+# ========================= EOF ====================================================================
