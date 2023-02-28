0b8afd92807bb8e7e9f6a2730c494c893cfe7659
dgketchum <dgketchum@gmail.com>
2023-02-28 15:41:38 -0700

    write cdl to arrays

--- field_points/analysis.py
+++ field_points/analysis.py
@@ -123,8 +123,7 @@ def correlations(state, npy_dir, out_dir, procs, calc):
     return ofile
 
 
-def partition_data(npy, out_dir, calc='simi'):
-
+def partition_data(npy, out_dir, calc='simi', classification='usbr'):
     periods = [(5, 1, 1),
                (6, 2, 1),
                (7, 2, 1),
@@ -132,24 +131,47 @@ def partition_data(npy, out_dir, calc='simi'):
                (9, 5, 4),
                (10, 7, 5)]
 
-    dt_range = [pd.to_datetime('{}-{}-01'.format(y, m)) for y in range(1987, 2022) for m in range(1, 13)]
+    if classification == 'cdl':
+        COLS.append('cdl')
+        dt_range = [pd.to_datetime('{}-{}-01'.format(y, m)) for y in range(2005, 2022) for m in range(1, 13)]
+    else:
+        dt_range = [pd.to_datetime('{}-{}-01'.format(y, m)) for y in range(1987, 2022) for m in range(1, 13)]
+
+    join_column, states = None, None
 
     for p in periods:
         rec, nrec = None, None
         month_end, met_time, ag_time = p
         desc = 'met{}_ag{}_fr{}'.format(met_time, ag_time, month_end)
         first = True
-        for state in BASIN_STATES[1:]:
 
-            npy_file = os.path.join(npy, '{}.npy'.format(state))
+        if classification == 'usbr':
+            states = ['CA', 'ID', 'MT', 'OR', 'WA', 'WY']
+            join_column = 'usbrid'
+        elif classification == 'itype':
+            states = ['CO', 'MT', 'NM', 'UT', 'WA', 'WY']
+            join_column = 'itype'
+        elif classification == 'time':
+            states = BASIN_STATES
+            join_column = 'index'
+        elif classification == 'cdl':
+            states = BASIN_STATES
+            join_column = 'index'
+
+        for state in states:
+
+            npy_file = os.path.join(npy, '{}.npy'.format(state, state))
             print(npy_file)
             js = npy_file.replace('.npy', '_index.json')
             with open(js, 'r') as fp:
                 dct = json.load(fp)
                 index = dct['index']
-                usbrid_ = dct['usbrid']
+                if classification == 'time':
+                    class_ = [int(i.split('_')[1]) for i in index]
+                else:
+                    class_ = dct[join_column]
 
-            n = int(np.ceil(len(index) / 1e4))
+            n = int(np.ceil(len(index) / 5e3))
             indx = split(index, n)
 
             data_mem = np.fromfile(npy_file)
@@ -161,8 +183,13 @@ def partition_data(npy, out_dir, calc='simi'):
 
                 months = np.multiply(np.ones((len(index[s_ind:e_ind]), len(dt_range))),
                                      np.array([dt.month for dt in dt_range]))
-                usbrid = np.repeat(np.array(usbrid_[s_ind:e_ind]).reshape((len(usbrid_[s_ind:e_ind]), 1)),
-                                   len(dt_range), axis=1)
+
+                if classification == 'cdl':
+                    classific = data[:, -len(dt_range):, -1]
+                    data = data[:, -len(dt_range):, :]
+                else:
+                    classific = np.repeat(np.array(class_[s_ind:e_ind]).reshape((len(class_[s_ind:e_ind]), 1)),
+                                          len(dt_range), axis=1)
 
                 if calc == 'simi':
                     kc = data[:, :, COLS.index('et')] / data[:, :, COLS.index('ietr')]
@@ -175,28 +202,43 @@ def partition_data(npy, out_dir, calc='simi'):
                 # depends on locally modified climate_indices package that takes cwb = ppt - pet as input to spei
                 cwb = data[:, :, COLS.index('ppt')] - data[:, :, COLS.index('etr')]
                 spei = np.apply_along_axis(lambda x: indices.spei(x, scale=met_time, **IDX_KWARGS), arr=cwb, axis=1)
-                stack = np.stack([simi[:, -len(dt_range):], spei[:, -len(dt_range):], months, usbrid])
+                stack = np.stack([simi[:, -len(dt_range):], spei[:, -len(dt_range):], months, classific])
                 d = stack[:, stack[2] == float(month_end)].reshape((4, len(index[s_ind:e_ind]), -1))
 
-                rec_ = d.copy()
-                rec_ = rec_[:2, rec_[-1] > 0]
-                nrec_ = d.copy()
-                nrec_ = nrec_[:2, nrec_[-1] == 0]
+                if classification == 'itype':
+                    d = d[:, :, -7:]
+
+                if classification == 'time':
+                    early = d[:, :, :10]
+                    late = d[:, :, -10:]
+                    if first:
+                        e = early.copy()
+                        l = late.copy()
+                        first = False
+                    else:
+                        e = np.append(e, early, axis=1)
+                        l = np.append(l, late, axis=1)
+                    print('{}, {} of {}'.format(state, i + 1, n))
+                    continue
 
                 if first:
-                    rec = rec_.copy()
-                    nrec = nrec_.copy()
+                    rec = d.copy()
                     first = False
                 else:
-                    rec = np.append(rec, rec_, axis=1)
-                    nrec = np.append(nrec, nrec_, axis=1)
+                    rec = np.append(rec, d, axis=1)
                 print('{}, {} of {}'.format(state, i + 1, n))
 
-        out_path = os.path.join(out_dir, '{}_rec.npy'.format(desc))
-        rec.tofile(out_path)
-        out_path = os.path.join(out_dir, '{}_nrec.npy'.format(desc))
-        nrec.tofile(out_path)
-        print('writing ', out_path)
+        if classification == 'time':
+            out_path = os.path.join(out_dir, '{}_early.npy'.format(desc))
+            e.tofile(out_path)
+            out_path = os.path.join(out_dir, '{}_late.npy'.format(desc))
+            l.tofile(out_path)
+            print('writing ', out_path)
+
+        else:
+            out_path = os.path.join(out_dir, '{}.npy'.format(desc))
+            rec.tofile(out_path)
+            print('writing ', out_path)
 
 
 if __name__ == '__main__':
@@ -207,11 +249,12 @@ if __name__ == '__main__':
     indir = os.path.join(root, 'field_pts/fields_data/fields_npy')
     odir = os.path.join(root, 'field_pts/indices')
 
-    # processes = 28
-    # for s in BASIN_STATES[1:]:
-    #     correlations(s, indir, odir, processes, calc='simi')
-    #     correlations(s, indir, odir, processes, calc='scui')
+    part = 'itype'
+    part_ = os.path.join(root, 'field_pts/fields_data/partitioned_npy/{}'.format(part))
+    # partition_data(indir, part_, classification=part)
 
-    part_ = os.path.join(root, 'field_pts/fields_data/partitioned_npy')
-    partition_data(indir, part_)
+    part = 'cdl'
+    indir = os.path.join(root, 'field_pts/fields_data/fields_cdl_npy')
+    part_ = os.path.join(root, 'field_pts/fields_data/partitioned_npy/{}'.format(part))
+    partition_data(indir, part_, classification=part)
 # ========================= EOF ====================================================================
