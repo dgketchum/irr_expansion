2b608b018a4c505134b3680b4633d48bb50cc8ee
dgketchum <dgketchum@gmail.com>
2023-02-28 15:41:21 -0700

    add cdl attribution

--- field_points/concatenate.py
+++ field_points/concatenate.py
@@ -4,38 +4,50 @@ import json
 import numpy as np
 import pandas as pd
 
-from itype_mapping import itype_integer_mapping
 from call_ee import BASIN_STATES
 
 COLS = ['et', 'cc', 'ppt', 'etr', 'eff_ppt', 'ietr']
 
 
-def concatenate_field_data(csv_dir, metadata, out, file_check=False, glob=None, itype_dir=None):
-    with open('field_points/tiles.json', 'r') as _file:
+def concatenate_field_data(csv_dir, metadata, out, file_check=False, glob=None, itype_dir=None, cdl_dir=None):
+    tile_file = 'field_points/tiles.json'
+    if not os.path.exists(tile_file):
+        tile_file = 'tiles.json'
+    with open(tile_file, 'r') as _file:
         tile_dct = json.load(_file)
 
-    itype_map = itype_integer_mapping()
-    for s in BASIN_STATES[7:]:
+    if cdl_dir:
+        COLS.append('CDL')
+
+    for s in BASIN_STATES[1:]:
 
         tiles = tile_dct[s]
         mdata = os.path.join(metadata, '{}.csv'.format(s))
         df = pd.read_csv(mdata, index_col='OPENET_ID')
         df = df.sort_index()
 
-        if s in ['CO', 'MT', 'UT', 'WY'] and itype_dir:
-            itype_f = os.path.join(itype_dir, '{}_openet_itype.csv'.format(s))
+        if s in ['CO', 'MT', 'UT', 'WA', 'WY'] and itype_dir:
+            itype_f = os.path.join(itype_dir, '{}.csv'.format(s.lower()))
             idf = pd.read_csv(itype_f, index_col='OPENET_ID')
             idf = idf[~idf.index.duplicated()]
-            idf['remap'] = idf['itype'].apply(lambda x: itype_map[x])
             match = [i for i in df.index if i in idf.index]
-            idf = idf.loc[match]
-            df['itype'] = [0 for _ in range(df.shape[0])]
-            df.loc[match, 'itype'] = idf.loc[match, 'remap']
-            df['itype'] = df['itype'].values.astype(int)
+            df.loc[match, 'itype'] = idf.loc[match, 'itype']
+        elif cdl_dir:
+            cdl_f = os.path.join(cdl_dir, '{}.csv'.format(s.lower()))
+            cdf = pd.read_csv(cdl_f, index_col='OPENET_ID')
+            cols = [c for c in cdf.columns]
+            cdf = cdf[~cdf.index.duplicated()]
+            match = [i for i in df.index if i in cdf.index]
+            df.loc[match, cols] = cdf.loc[match, cols]
+            df = df[cols]
+            # dummy data for pre-2008 years needed to calc drought metrics
+            for y in range(2005, 2008):
+                df['CROP_{}'.format(y)] = [0 for _ in range(df.shape[0])]
         else:
             df['itype'] = [0 for _ in range(df.shape[0])]
 
         dt_range = ['{}-{}-01'.format(y, m) for y in range(1984, 2022) for m in range(1, 13)]
+
         data = np.zeros((df.shape[0], len(dt_range), len(COLS)))
 
         print('\n', s)
@@ -59,13 +71,19 @@ def concatenate_field_data(csv_dir, metadata, out, file_check=False, glob=None, 
         for dtstr, csv_list in file_dct.items():
             tdf = None
             y, m = int(dtstr[:4]), int(dtstr[5:])
+
+            if cdl_dir and y < 2005:
+                continue
+
             dt_ind = dt_range.index('{}-{}-01'.format(y, m))
             first = True
             for csv in csv_list:
                 c = pd.read_csv(csv, index_col='OPENET_ID')
                 match = [i for i in c.index if i in df.index]
                 c = c.loc[match]
-                print(c.shape, csv)
+
+                if cdl_dir:
+                    c.loc[match, 'CDL'] = df.loc[match, 'CROP_{}'.format(y)]
 
                 if m not in range(4, 11) or y < 1987:
                     c[['et', 'cc', 'eff_ppt', 'ietr']] = np.zeros((c.shape[0], 4))
@@ -77,14 +95,23 @@ def concatenate_field_data(csv_dir, metadata, out, file_check=False, glob=None, 
                     tdf = pd.concat([tdf, c])
 
             tdf = tdf[COLS]
+            tdf = tdf.sort_index()
+            assert np.all(df.index == tdf.index)
             data[:, dt_ind, :] = tdf.values
 
         out_path = os.path.join(out, '{}.npy'.format(s))
         data.tofile(out_path)
         out_js = out_path.replace('.npy', '_index.json')
-        dct = {'index': list(df.index), 'usbrid': list(df['usbrid']), 'itype': list(df['itype'])}
+
+        if itype_dir:
+            dct = {'index': list(df.index), 'usbrid': list(df['usbrid']), 'itype': list(df['itype'])}
+
+        elif cdl_dir:
+            dct = {'index': list(df.index)}
+
         with open(out_js, 'w') as fp:
             json.dump(dct, fp, indent=4)
+
         print(out_path)
 
 
@@ -94,9 +121,16 @@ if __name__ == '__main__':
         root = '/home/dgketchum/data'
 
     csv_ = os.path.join(root, 'field_pts/csv/fields')
-    fpd = os.path.join(root, 'field_pts/fields_data/fields_npy')
     meta_ = os.path.join(root, 'field_pts/fields_data/fields_shp')
+
     itype_data = '/media/research/IrrigationGIS/expansion/tables/itype'
+    cdl_data = '/media/research/IrrigationGIS/expansion/tables/cdl'
+
+    # fpd = os.path.join(root, 'field_pts/fields_data/fields_npy')
+    # concatenate_field_data(csv_, meta_, fpd, glob='ietr_fields_16FEB2023',
+    #                        file_check=False, itype_dir=itype_data)
+
+    fpd = os.path.join(root, 'field_pts/fields_data/fields_cdl_npy')
     concatenate_field_data(csv_, meta_, fpd, glob='ietr_fields_16FEB2023',
-                           file_check=False, itype_dir=itype_data)
+                           file_check=False, cdl_dir=cdl_data)
 # ========================= EOF ====================================================================
