8f7ef1f787d6381eb67ae726e4d0f7a0a2b29515
dgketchum <dgketchum@gmail.com>
2023-03-02 12:08:12 -0700

    write state price time series, annual

--- field_points/crops.py
+++ field_points/crops.py
@@ -171,33 +171,73 @@ def map_nass_to_csl(values_dir, txt):
     pass
 
 
-def get_price_timeseries(dir_, mapping, out_csv):
+def get_price_timeseries(dir_, mapping, out_js):
     st_abv = state_name_abbreviation()
-    full_states = [st_abv[k] for k in BASIN_STATES]
+    inv_st_abv = {v: k for k, v in st_abv.items()}
     cdl = study_area_crops()
     m = pd.read_csv(mapping)
+    m['table'] = m['file'].apply(lambda x: x.split('.')[0][-3:])
     m.index = m['cdl_code']
-    m.drop(columns=['Unnamed: 0', 'cdl_code'], inplace=True)
-    files = m.T.to_dict()
-    dct = {}
-    for code, crop in cdl.items():
-        if code not in files.keys():
+    m.drop(columns=['Unnamed: 0', 'cdl_code', 'file', 'desc'], inplace=True)
+    tables = m.T.to_dict()
+    dct, target_year = {}, {}
+    for i, (code, (crop, count)) in enumerate(cdl.items()):
+        if code not in tables.keys():
             continue
-        for year in range(2009, 2022):
+        table = tables[code]['table']
+        dct[code] = {s: (crop, []) for s in BASIN_STATES}
+        years = list(range(2003, 2023))
+        years.reverse()
+        for year in years:
+            if year > 2019:
+                target_col = 5
+            elif table == '004':
+                target_col = 4
+            else:
+                target_col = 3
             d = [x for x in os.listdir(dir_) if x.endswith(str(year))][0]
             d = os.path.join(dir_, d)
-            t = [x for x in os.listdir(d) if files[code]['file'].split('_')[-1].replace('t', '') in x][0]
+
+            try:
+                t = [x for x in os.listdir(d) if table in x][0]
+            except IndexError:
+                print(i, year, crop, count, 'no table')
+                continue
+
             f = os.path.join(d, t)
-            c = pd.read_csv(f, skiprows=4, encoding_errors='ignore', header=None)
-            c['state'] = c[2]
-            c['yr_col'] = c[5]
-            if 'AZ' in c['state'].values:
-                c = c.loc[c['state'].apply(lambda x: True if x in BASIN_STATES else False)]
-            else:
-                c['state'] = [x.strip() if str(x).strip() in full_states else 'None' for x in list(c['state'])]
-                c = c.loc[c['state'].apply(lambda x: True if x in full_states else False)]
-            c = c[['state', 'yr_col']]
-            print(list(c.columns))
+
+            try:
+
+                if table in ['004', '007', '002']:
+                    c = pd.read_csv(f, skiprows=9, encoding_errors='ignore', header=None)
+                    ind = list(c[2]).index(process.extractOne(crop, choices=c[2])[0])
+                    p = float(c.loc[ind, 4])
+                    for s in BASIN_STATES:
+                        dct[code][s][1].append((year, float(p)))
+                else:
+                    c = pd.read_csv(f, skiprows=7, encoding_errors='ignore', header=None)
+                    c['state'] = c[2]
+                    c['yr_col'] = c[target_col]
+                    if 'AZ' in c['state'].values:
+                        c = c.loc[c['state'].apply(lambda x: True if x in BASIN_STATES else False)]
+                    else:
+                        c['state'] = [inv_st_abv[x.strip()] if str(x).strip() in inv_st_abv.keys()
+                                      else 'None' for x in list(c['state'])]
+                        c = c.loc[c['state'].apply(lambda x: True if x in BASIN_STATES else False)]
+                    c = c[['state', 'yr_col']]
+
+                    for s, p in zip(c['state'], c['yr_col']):
+                        try:
+                            dct[code][s][1].append((year, float(p)))
+                        except ValueError:
+                            dct[code][s][1].append((year, np.nan))
+
+            except pd.errors.ParserError as e:
+                print(i, year, crop)
+                pass
+
+    with open(out_js, 'w') as fp:
+        json.dump(dct, fp, indent=4)
 
 
 if __name__ == '__main__':
@@ -205,23 +245,10 @@ if __name__ == '__main__':
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS'
 
-    fields_openet = '/media/research/IrrigationGIS/openET/OpenET_GeoDatabase_cdl'
-    extract = '/media/nvm/field_pts/csv/cdl'
-    cdl_csv = '/media/research/IrrigationGIS/expansion/tables/cdl/crops'
-    # get_openet_cdl(fields_openet, extract, cdl_csv)
-
-    key_ = '/home/dgketchum/quickstats_token.json'
-    # get_crop_values(key_)
-
-    met_cdl = '/media/nvm/field_pts/fields_data/partitioned_npy/cdl/met4_ag3_fr8.npy'
-    transistion = '/media/research/IrrigationGIS/expansion/analysis/transition'
-    # transition_probability(met_cdl, transistion)
-
-    cdl_csv = '/media/research/IrrigationGIS/expansion/tables/cdl/accuracy/acc.json'
-    # cdl_accuracy(cdl_csv)
-
     price_data = '/media/research/IrrigationGIS/expansion/tables/crop_value/nass_data'
-    price_ts = '/media/research/IrrigationGIS/expansion/tables/crop_value/time_series.csv'
     map_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/values.csv'
+    # map_nass_to_csl(price_data, map_)
+
+    price_ts = '/media/research/IrrigationGIS/expansion/tables/crop_value/time_series.json'
     get_price_timeseries(price_data, map_, price_ts)
 # ========================= EOF ====================================================================
