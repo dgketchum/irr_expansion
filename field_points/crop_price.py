cdfed0814517c979f425172eae35541221aea80d
dgketchum <dgketchum@gmail.com>
2023-03-06 21:31:01 -0700

    write monthly nominal price

--- field_points/crop_price.py
+++ field_points/crop_price.py
@@ -7,12 +7,13 @@ from fuzzywuzzy import process
 from nasspython.nass_api import nass_data
 
 from gridded_data import BASIN_STATES
-from utils.cdl import study_area_crops, nass_price_queries
+from utils.cdl import nass_monthly_price_queries, nass_annual_price_queries
+from utils.cdl import study_area_crops, ppi_to_cdl_code
 from utils.placenames import state_name_abbreviation
 
 
-def get_annual_price_timeseries(dir_, mapping, out_js):
-    st_abv = state_name_abbreviation()
+def get_annual_price_timeseries(dir_, mapping, out_js, key):
+    st_abv, nass_queries = state_name_abbreviation(), nass_annual_price_queries()
     inv_st_abv = {v: k for k, v in st_abv.items()}
     cdl = study_area_crops()
     m = pd.read_csv(mapping)
@@ -25,7 +26,7 @@ def get_annual_price_timeseries(dir_, mapping, out_js):
         if code not in tables.keys():
             continue
         table = tables[code]['table']
-        dct[code] = {s: (crop, []) for s in BASIN_STATES}
+        dct[code] = {s: [] for s in BASIN_STATES}
         years = list(range(2003, 2023))
         for year in years:
             if year > 2019:
@@ -52,25 +53,31 @@ def get_annual_price_timeseries(dir_, mapping, out_js):
                     ind = list(c[2]).index(process.extractOne(crop, choices=c[2])[0])
                     p = float(c.loc[ind, 4])
                     for s in BASIN_STATES:
-                        dct[code][s][1].append((year, float(p)))
+                        dct[code][s].append(('{}-07-01'.format(year), float(p)))
                         dct[code]['national'] = True
+
+                # elif crop in ['Rye', 'Mint']:
+                #     # TODO: re-run when re-authorized
+                #     query = nass_queries[crop]
+                #     query['year'] = year
+                #     resp = nass_data(key, **query)
+
                 else:
                     c = pd.read_csv(f, skiprows=7, encoding_errors='ignore', header=None)
                     c['state'] = c[2]
                     c['yr_col'] = c[target_col]
-                    if 'AZ' in c['state'].values:
+                    if 'CA' in c['state'].values:
                         c = c.loc[c['state'].apply(lambda x: True if x in BASIN_STATES else False)]
                     else:
                         c['state'] = [inv_st_abv[x.strip()] if str(x).strip() in inv_st_abv.keys()
                                       else 'None' for x in list(c['state'])]
                         c = c.loc[c['state'].apply(lambda x: True if x in BASIN_STATES else False)]
                     c = c[['state', 'yr_col']]
-
                     for s, p in zip(c['state'], c['yr_col']):
                         try:
-                            dct[code][s][1].append((year, float(p)))
+                            dct[code][s].append(('{}-07-01'.format(year), float(p)))
                         except ValueError:
-                            dct[code][s][1].append((year, np.nan))
+                            dct[code][s].append(('{}-07-01'.format(year), np.nan))
                         dct[code]['national'] = False
 
             except pd.errors.ParserError as e:
@@ -79,13 +86,14 @@ def get_annual_price_timeseries(dir_, mapping, out_js):
 
         print(code, crop, 'success')
 
+    dct = {cdl[k][0]: v for k, v in dct.items()}
     with open(out_js, 'w') as fp:
         json.dump(dct, fp, indent=4)
 
 
 def get_monthly_price_timeseries(key, out_js):
     state_abv = state_name_abbreviation()
-    queries = nass_price_queries()
+    queries = nass_monthly_price_queries()
 
     with open(key, 'r') as fp:
         key = json.load(fp)['auth']
@@ -110,51 +118,102 @@ def get_monthly_price_timeseries(key, out_js):
         json.dump(dct, fp, indent=4)
 
 
-def prepare_deflated_price_data(monthly_price, annual_price, ppi_data, out_csv):
-    pass
+def _from_series(tseries, state):
+    try:
+        yrs = [int(x[0][:4]) for x in tseries[state]]
+        data = [float(x[1]) for x in tseries[state]]
+        ind = [pd.to_datetime('{}-07-01'.format(y)) for y in yrs]
+    except ValueError:
+        ind = [pd.to_datetime('{}-07-01'.format(y)) for y in range(2008, 2022)]
+        data = [np.nan for x in ind]
+    series = pd.Series(index=ind, data=data)
+    return series
 
 
-def find_annual_price_tables(values_dir, txt):
-    cdl = study_area_crops()
-    df = pd.read_table(txt, header=None)
-    df.columns = ['index', 'file', 'desc']
-    df.drop(columns=['index'], inplace=True)
-    df['desc'] = df['desc'].apply(lambda x: x.split('Price per')[0])
+def join_price_data(monthly_price, annual_price, nominal):
+    st_names = state_name_abbreviation()
+    st_abv = {v.upper(): k for k, v in st_names.items() if k in BASIN_STATES}
 
-    dct_choices = sorted([v[0] for k, v in cdl.items() if len(v[0]) > 3 and v[1] > 1000])
-    inv_cdl = {v[0]: k for k, v in cdl.items()}
-    files = ['cpvl_p10_t004.csv', 'cpvl_p08_t002.csv', 'cpvl_p13_t007.csv']
+    with open(monthly_price, 'r') as fp:
+        month_price = json.load(fp)
+    month_price = {k: {st_abv[kk]: vv for kk, vv in month_price[k].items()} for k, v in month_price.items()}
 
-    for f in files:
+    with open(annual_price, 'r') as fp:
+        annual_price = json.load(fp)
 
-        table = pd.read_csv(os.path.join(values_dir, 'CropValuSu-02-24-2017', f),
-                            skiprows=6, encoding_errors='ignore')
+    counts = study_area_crops()
+    counts = {k: v for k, v in counts.items() if v[0] in month_price.keys() or v[0] in annual_price.keys()}
+    dt_range = [pd.to_datetime('{}-{}-01'.format(y, m)) for y in range(2008, 2022) for m in range(1, 13)]
+    start, end = dt_range[0], dt_range[-1]
+    missing_state = None
+
+    for code, (crop, count) in counts.items():
+
+        if crop in ['Rye']:
+            continue
+
+        print(crop)
+
         try:
-            table.columns = ['4', 'h', 'name', 'unit', 'y_minus_3', 'y_minus_2', 'y_minus_1']
-        except ValueError:
-            table.columns = ['4', 'h', 'name', 'y_minus_3', 'y_minus_2', 'y_minus_1']
+            ts = month_price[crop]
+            monthly, annual, national = True, False, False
+        except KeyError:
+            ts = annual_price[crop]
+            monthly, annual = False, True
+            national = ts['national']
 
-        table = table[['name', 'y_minus_3', 'y_minus_2', 'y_minus_1']]
-        table.dropna(subset=['name'], inplace=True)
-        table.dropna(subset=['y_minus_3', 'y_minus_2', 'y_minus_1'], how='all', inplace=True)
+        c = pd.DataFrame(columns=[s for s in BASIN_STATES])
+        c = c.reindex(dt_range)
+        c = c.loc['2008-01-01': '2021-12-31']
 
-        for i, r in table.iterrows():
-            approx = process.extractOne(r['name'], choices=dct_choices)
-            if approx[1] > 85:
-                print('match {} == {}'.format(r['name'], approx[0]))
-                test_exists = process.extractOne(r['name'], choices=df['desc'])
-                if test_exists[1] > 85:
-                    print('already in database, skipping')
-                else:
-                    print('appending')
-                    df = df.append({'file': f, 'desc': approx[0]}, ignore_index=True)
-            else:
-                print('nonmatch {} == {}'.format(r['name'], approx[0]))
+        if annual and national:
+            series = _from_series(ts, 'CA')
+            for s in BASIN_STATES:
+                c[s] = series
 
-    df['cdl_name'] = df['desc'].apply(lambda x: process.extractOne(x, choices=dct_choices)[0])
-    df['cdl_code'] = df['desc'].apply(lambda x: inv_cdl[process.extractOne(x, choices=dct_choices)[0]])
-    df.to_csv('values_.csv')
-    pass
+        missing_state = []
+        for state in BASIN_STATES:
+
+            st_data = ts[state]
+
+            if annual and not national:
+                pass
+
+            for dt, val in st_data:
+                pdt = pd.to_datetime(dt)
+                if end < pdt or pdt < start:
+                    continue
+                try:
+                    c.loc[pdt, state] = float(val)
+                except ValueError:
+                    c.loc[pdt, state] = np.nan
+
+            missing = [i for i, r in c[state].items() if np.isnan(r)]
+            if missing:
+                ts = annual_price[crop]
+                series = _from_series(ts, state)
+                series = series[~series.index.duplicated()]
+                series = series.reindex(dt_range).interpolate()
+                series = series.dropna()
+
+                try:
+                    c.loc[missing, state] = series.loc[missing].values
+                except KeyError:
+                    c[state] = c[state].values.astype(float)
+                    c[state] = c[state].interpolate(method='linear').ffill().bfill()
+                    if np.all(np.isnan(c[state].values)):
+                        missing_state.append(state)
+
+        if missing_state:
+            mean_ = c.mean(axis=1)
+            for state in missing_state:
+                c[state] = mean_
+        print(c.shape, '\n')
+
+        if 'Other' in crop:
+            crop = crop.replace(' ', '_').replace('/', '_')
+        o_file = os.path.join(nominal, '{}.csv'.format(crop))
+        c.to_csv(o_file)
 
 
 if __name__ == '__main__':
@@ -164,11 +223,16 @@ if __name__ == '__main__':
 
     key_ = '/home/dgketchum/quickstats_token.json'
     nass_price_monthly = '/media/research/IrrigationGIS/expansion/analysis/nass_price_monthly.json'
-    get_monthly_price_timeseries(key_, nass_price_monthly)
+    # get_monthly_price_timeseries(key_, nass_price_monthly)
 
     price_data = '/media/research/IrrigationGIS/expansion/tables/crop_value/nass_data'
-    map_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/values.csv'
+    map_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/nass_data/values.csv'
     nass_price_annual = '/media/research/IrrigationGIS/expansion/analysis/nass_price_annual.json'
-    get_annual_price_timeseries(price_data, map_, nass_price_annual)
+    # get_annual_price_timeseries(price_data, map_, nass_price_annual, key_)
+
+    nominal_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/nominal'
+    join_price_data(nass_price_monthly, nass_price_annual, nominal_)
+
+    ppi_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/ppi_cdl_monthly.csv'
 
 # ========================= EOF ====================================================================
