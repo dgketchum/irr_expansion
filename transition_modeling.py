139d78cffd3c253ea45c1453b6aab1408f201a27
dgketchum <dgketchum@gmail.com>
2023-03-09 15:01:57 -0700

    find a characteristic time scale for every crop transition

--- transition_modeling.py
+++ transition_modeling.py
@@ -1,25 +1,25 @@
-import os
 import json
-import random
+import os
 import pickle
-import tempfile
+import random
 from itertools import combinations
+from dateutil.relativedelta import relativedelta as reldt
 
+import aesara
+import aesara.tensor as tt
+import arviz as az
 import numpy as np
 import pandas as pd
 import pymc as pm
 import pymc.sampling_jax
-import aesara
-import aesara.tensor as tt
-import arviz as az
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 from utils.cdl import cdl_key
 
 DEFAULTS = {'draws': 1000,
             'tune': 1000}
 
+KEYS = [1, 12, 21, 23, 24, 28, 36, 37, 41, 42, 43, 47, 49, 53, 56, 57, 58, 59, 66, 68, 69, 71, 77]
 
 def _open_js(file_):
     with open(file_, 'r') as fp:
@@ -27,7 +27,7 @@ def _open_js(file_):
     return dct
 
 
-def dirichlet_regression(climate, from_price, to_price, from_crop=24, save_model=None):
+def load_data(climate, to_price, from_price, from_crop, n_samples=100):
     climate = _open_js(climate)
     to_price = _open_js(to_price)
     from_price = _open_js(from_price)
@@ -44,34 +44,69 @@ def dirichlet_regression(climate, from_price, to_price, from_crop=24, save_model
     labels = [i for l in labels for i in l]
 
     combo = list(zip(c_data, fp_data, tp_data, labels))
+    len_ = len(combo)
+    combo = [c for c in combo if np.all(np.isfinite(c))]
+    print('dropped {} of {}'.format(len_ - len(combo), len_))
     random.shuffle(combo)
-    c_data[:], fp_data[:], tp_data[:], labels[:] = zip(*combo[:1000])
+    if n_samples:
+        c_data[:], fp_data[:], tp_data[:], labels[:] = zip(*combo[:n_samples])
+    else:
+        c_data[:], fp_data[:], tp_data[:], labels[:] = zip(*combo)
 
-    s = pd.Series(labels)
-    obs = pd.get_dummies(s).values
-    n, k = obs.shape
-
-    climate = aesara.shared(np.array(c_data))
-
-    with pm.Model() as dmr_model:
-        a = pm.Normal('a', mu=0, sigma=1, shape=k)
-        b = pm.Normal('b', mu=0, sigma=1, shape=k)
-
-        alpha = pm.Deterministic('alpha', pm.math.exp(a + tt.outer(climate, b)))
-
-        p = pm.Dirichlet('p', a=alpha, shape=(n, k))
-
-        F = pm.Multinomial('F', 1, p, observed=obs)
-
-        trace = pm.sampling_jax.sample_numpyro_nuts(**DEFAULTS)
-
-        if save_model:
-            with open(save_model, 'wb') as buff:
-                pickle.dump({'trace': trace}, buff)
-                print('saving', save_model)
+    return c_data, fp_data, tp_data, labels, target_keys
 
 
-def summarize_dirichlet_model(saved_model):
+def logistic_regression(climate, from_price, to_price, from_crop=24, samples=10000):
+    split = int(np.floor(0.7 * samples))
+    c_data, fp_data, tp_data, labels, keys = load_data(climate, from_price, to_price,
+                                                 from_crop=from_crop, n_samples=samples)
+
+    x, y = np.array([c_data[:split], fp_data[:split], tp_data[:split]]).T, np.array(labels[:split])
+    x_test, y_test = np.array([c_data[split:], fp_data[split:], tp_data[split:]]).T, np.array(labels[split:])
+
+    clf = LogisticRegression().fit(x, y)
+    pred = clf.predict(x_test)
+    df = pd.DataFrame(columns=['pred', 'y'], data=np.array([pred, y_test]).T)
+    df = df.loc[df['y'] != 24]
+    df['correct'] = df[df['y'] == df['pred']]
+    pass
+
+
+def dirichlet_regression(climate, from_price, to_price, save_model=None):
+
+    for fc in KEYS:
+        c_data, fp_data, tp_data, labels, keys = load_data(climate, from_price, to_price, from_crop=fc)
+        s = pd.Series(labels)
+        obs = pd.get_dummies(s).values
+        # obs = obs.sum(axis=0)
+        n, k = obs.shape
+
+        climate = aesara.shared(np.array(c_data))
+        fprice = aesara.shared(np.array(fp_data))
+        tprice = aesara.shared(np.array(tp_data))
+
+        with pm.Model() as dmr_model:
+            a = pm.Normal('a', mu=0, sigma=1, shape=k)
+            b = pm.Normal('b', mu=0, sigma=1, shape=k)
+            b = pm.Normal('c', mu=0, sigma=1, shape=k)
+
+            alpha = pm.Deterministic('alpha', pm.math.exp(a + tt.outer(tprice, b) + tt.outer(climate, c)))
+
+            p = pm.Dirichlet('p', a=alpha, shape=(n, k))
+
+            dm = pm.DirichletMultinomial('dm', 1, p, observed=obs)
+
+            pm.model_to_graphviz(dmr_model).view()
+
+            trace = pm.sampling_jax.sample_numpyro_nuts(**DEFAULTS)
+
+            if save_model:
+                with open(save_model.format(fc), 'wb') as buff:
+                    pickle.dump({'trace': trace}, buff)
+                    print('saving', save_model)
+
+
+def summarize_pymc_model(saved_model):
     with open(saved_model, 'rb') as buff:
         mdata = pickle.load(buff)
         trace = mdata['trace']
@@ -79,7 +114,7 @@ def summarize_dirichlet_model(saved_model):
     pass
 
 
-def crop_transitions(cdl_npy, price_files, out_matrix):
+def crop_transitions(cdl_npy, price_files, response_timescale, out_matrix):
     with open(price_files, 'r') as fp:
         dct = json.load(fp)
 
@@ -89,6 +124,9 @@ def crop_transitions(cdl_npy, price_files, out_matrix):
         df = df.mean(axis=1)
         df = df.to_dict()
         prices[int(code)] = df.copy()
+
+    with open(response_timescale, 'r') as fp:
+        timescale = json.load(fp)
 
     cdl = cdl_key()
     time_series_length = 17
@@ -116,17 +154,21 @@ def crop_transitions(cdl_npy, price_files, out_matrix):
                 continue
 
             fc, tc = int(c[3]), int(v.T[e + 1, 3])
-            trans = '{}_{}'.format(fc, tc)
 
             if not (fc in set_ and tc in set_):
                 continue
 
+            trans = '{}_{}'.format(fc, tc)
+            fl, tl = timescale[trans]['flag'], timescale[trans]['tlag']
+            y = e + 2005
+            ref_time = pd.to_datetime('{}-05-01'.format(y))
+            fprice_lag_str = (ref_time - reldt(months=fl)).strftime('%Y-%m-%d')
+            tprice_lag_str = (ref_time - reldt(months=tl)).strftime('%Y-%m-%d')
+            fprice[trans].append(prices[fc][fprice_lag_str])
+            tprice[trans].append(prices[tc][tprice_lag_str])
+
             spei[trans].append(c[1])
 
-            y = e + 2005
-            price_lag_str = '{}-07-01'.format(y - 1)
-            fprice[trans].append(prices[fc][price_lag_str])
-            tprice[trans].append(prices[tc][price_lag_str])
             ct += 1
 
         if i % 10000 == 0.:
@@ -144,17 +186,20 @@ def crop_transitions(cdl_npy, price_files, out_matrix):
 
 
 if __name__ == '__main__':
+
     met_cdl = '/media/nvm/field_pts/fields_data/partitioned_npy/cdl/met4_ag3_fr8.npy'
     transitions_ = '/media/research/IrrigationGIS/expansion/analysis/transition'
     files_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/price_files.json'
-    # crop_transitions(met_cdl, files_, transitions_)
+    response_timescale_ = '/media/research/IrrigationGIS/expansion/analysis/transition/time_scales.json'
+
+    crop_transitions(met_cdl, files_, response_timescale_, transitions_)
 
     fp = os.path.join(transitions_, 'fprice.json')
     tp = os.path.join(transitions_, 'tprice.json')
     c = os.path.join(transitions_, 'spei.json')
-    target = 24
-    model_dst = os.path.join(transitions_, 'model_{}.pkl'.format(target))
-    dirichlet_regression(c, fp, tp, from_crop=target, save_model=model_dst)
+    model_dst = os.path.join(transitions_, 'model_{}.pkl')
+    # dirichlet_regression(c, fp, tp, from_crop=target, save_model=model_dst)
+    # logistic_regression(c, fp, tp, from_crop=target)
 
-    # summarize_dirichlet_model(model_dst)
+    # summarize_pymc_model(model_dst)
 # ========================= EOF ====================================================================
