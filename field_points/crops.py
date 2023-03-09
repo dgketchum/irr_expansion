9f8ec312ae44e5eb23815f2c9583faf1f45dbdeb
dgketchum <dgketchum@gmail.com>
2023-03-08 18:01:48 -0700

    refactor transition probability functions to their own module

--- field_points/crops.py
+++ field_points/crops.py
@@ -1,7 +1,6 @@
 import json
 import os
 from collections import OrderedDict
-from itertools import combinations
 from pprint import pprint
 
 import numpy as np
@@ -54,81 +53,6 @@ def get_openet_cdl(in_dir, join_csv, out_dir):
     pprint(dct)
 
 
-def transition_probability(cdl_npy, prices, out_matrix, glob='2MAR2023'):
-    counts, set_ = None, None
-    cdl = cdl_key()
-
-    time_series_length = 17
-    df = None
-
-    rec = np.fromfile(cdl_npy, dtype=float).reshape((4, -1, time_series_length))
-    set_, classes = list(df.columns), [cdl[c][0] for c in df.columns]
-
-    set_ = [s for s in set_ if s > 0]
-    keys = list((combinations(set_, 2)))
-    opposites = [(s, f) for f, s in keys]
-    [keys.append(o) for o in opposites]
-    [keys.append((i, i)) for i in set_]
-    keys = [(int(a), int(b)) for a, b in keys]
-    rec = np.moveaxis(rec, 0, 1)
-
-    counts = {k: 0 for k in keys}
-    from_crop = {k: [] for k in keys}
-    to_crop = {k: [] for k in keys}
-    spei = {k: [] for k in keys}
-
-    df = df.to_dict()
-    df = {k: {kk.strftime('%Y-%m-%d'): vv for kk, vv in v.items()} for k, v in df.items()}
-
-    ct = 0
-    for i, v in enumerate(rec):
-        for e, c in enumerate(v.T):
-            # skip last year of series, and first three (null) years
-            if e == 16 or e < 2:
-                continue
-
-            fc, tc = int(c[3]), int(v.T[e + 1, 3])
-            trans = (fc, tc)
-            if not (trans[0] in set_ and trans[1] in set_):
-                continue
-
-            y = e + 2005
-            counts[trans] += 1
-            dtstr = '{}-01-01'.format(y)
-            from_crop[trans].append(df[fc][dtstr])
-            to_crop[trans].append(df[tc][dtstr])
-            spei[trans].append(c[1])
-            ct += 1
-            if fc != tc:
-                pass
-
-        if i % 10000 == 0.:
-            print(i)
-
-    print('{} transtions'.format(ct))
-    map = np.zeros((len(classes), len(classes)))
-    for r, c in counts.keys():
-        map[set_.index(r), set_.index(c)] = counts[r, c]
-
-    prob = np.divide(map, np.sum(map, axis=0))
-
-    tdf = pd.DataFrame(columns=classes, index=classes, data=prob)
-    out_file = os.path.join(out_matrix, 'transitions_{}_crop.csv'.format(len(set_)))
-    tdf.to_csv(out_file, float_format='%.3f')
-
-    tdf = pd.DataFrame(columns=classes, index=classes, data=map)
-    out_file = os.path.join(out_matrix, 'transitions_{}_crop_counts.csv'.format(len(set_)))
-    tdf.to_csv(out_file, float_format='%.0f')
-
-    outs = zip([counts, from_crop, to_crop, spei], ['counts', 'fc_ppi', 'tc_ppi', 'spei'])
-    for d, js in outs:
-        d = {'{}'.format(k): v for k, v in d.items()}
-        out_js = os.path.join(out_matrix, '{}_{}.json'.format(js, glob))
-        with open(out_js, 'w') as fp:
-            json.dump(d, fp, indent=4)
-    pass
-
-
 def cdl_area_timeseries(cdl_dir, area_json, out_json):
     with open(area_json, 'r') as _file:
         areas = json.load(_file)
@@ -169,14 +93,9 @@ if __name__ == '__main__':
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS'
 
-    met_cdl = '/media/nvm/field_pts/fields_data/partitioned_npy/cdl/met4_ag3_fr8.npy'
-    transistion = '/media/research/IrrigationGIS/expansion/analysis/transition'
-    ppi_ = '/media/research/IrrigationGIS/expansion/tables/crop_value/ppi_cdl_monthly.csv'
-    # transition_probability(met_cdl, ppi_, transistion)
-
     crops = '/media/research/IrrigationGIS/expansion/tables/cdl/crops'
     areas_ = '/media/research/IrrigationGIS/expansion/tables/cdl/fields_area.json'
-    cld_area_ = '/media/research/IrrigationGIS/expansion/tables/cdl/cdl_area_timesereies.json'
-    cdl_area_timeseries(crops, areas_, cld_area_)
+    cdl_area_ = '/media/research/IrrigationGIS/expansion/tables/cdl/cdl_area_timesereies.json'
+    cdl_area_timeseries(crops, areas_, cdl_area_)
 
 # ========================= EOF ====================================================================
