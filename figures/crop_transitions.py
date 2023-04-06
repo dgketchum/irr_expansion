ee283cddf32843c95d5fbab3e18b10102489e349
dgketchum <dgketchum@gmail.com>
2023-04-05 21:13:32 -0600

    whisker plot of coefficient values

--- figures/crop_transitions.py
+++ figures/crop_transitions.py
@@ -1,22 +1,19 @@
 import os
+import json
 from itertools import combinations
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import arviz as az
 from field_points.crop_codes import cdl_key
 from transition_modeling.transition_data import KEYS
 
 
 def transition_probability(cdl_npy, out_matrix):
-    counts, set_ = None, None
     cdl = cdl_key()
-
     time_series_length = 17
-    df = None
-
     rec = np.fromfile(cdl_npy, dtype=float).reshape((4, -1, time_series_length))
     set_, classes = KEYS, [cdl[c][0] for c in KEYS]
 
@@ -34,13 +31,14 @@ def transition_probability(cdl_npy, out_matrix):
 
     for i, v in enumerate(rec):
 
+        if np.any(np.isnan(v)):
+            continue
+
         if i % 10000 == 0 and i > 1:
             print('{} of {}'.format(i, rec.shape[0]))
 
-
         for e, c in enumerate(v.T):
-            if np.any(np.isnan(c)):
-                continue
+
             if e == 16:
                 continue
             if c[1] >= 0:
@@ -61,21 +59,25 @@ def transition_probability(cdl_npy, out_matrix):
             from_ct['All'][trans[0]] += 1
 
     for k, d in dct.items():
-        map = np.zeros((len(KEYS), len(KEYS)))
+        map_ = np.zeros((len(KEYS), len(KEYS)))
         for r, c in d.keys():
-            map[KEYS.index(r), KEYS.index(c)] = d[r, c]
+            map_[KEYS.index(r), KEYS.index(c)] = d[r, c]
 
-        prob = np.divide(map, np.sum(map, axis=0))
+        prob = map_ / np.sum(map_, axis=1)[:, np.newaxis]
+        prob[np.isnan(prob)] = 0.0
+
         fig = plt.figure(figsize=(16, 10))
         ax = sns.heatmap(prob, square=False, annot=True, cmap='rocket_r', cbar=False,
                          xticklabels=classes, yticklabels=classes, fmt='.2f')
         ax2 = ax.twinx()
+        ax2.set_ylim(ax.get_ylim())
+        ytick_labels = ['n = {:,}'.format(from_ct[k][key]) for key in KEYS]
+        ax2.set_yticklabels(ytick_labels)
         ax2.set_yticks(ax.get_yticks())
-        ax2.set_yticklabels(['n = {:,}'.format(v) for k, v in from_ct[k].items()])
-        plt.yticks(rotation=0)
+        ax2.tick_params(axis='y', which='both', pad=10)
 
         if k == 'All':
-            plt.suptitle('{} Year Crop Transition Probabilities'.format(k))
+            plt.suptitle('Crop Transition Probabilities')
             fig_file = os.path.join(out_matrix, 'transition_heatmap_{}.png'.format(k))
 
         else:
@@ -83,9 +85,62 @@ def transition_probability(cdl_npy, out_matrix):
             fig_file = os.path.join(out_matrix, 'transition_heatmap_{}.png'.format(k))
 
         plt.tight_layout()
-        plt.show()
         plt.savefig(fig_file, bbox_inches='tight')
         plt.close()
+
+
+def transition_model(summaries, traces, figures, n_include=5):
+    cdl = cdl_key()
+    predominant_crops = [36, 1, 21, 24, 23]
+
+    for code in KEYS:
+        sfile = os.path.join(summaries, 'model_sft_{}.csv'.format(code))
+        try:
+            df = pd.read_csv(sfile, index_col=0)
+        except FileNotFoundError:
+            print('{} not found'.format(sfile))
+            continue
+        counts = [(r['label'], int(r['counts'])) for i, r in df.iterrows() if r['coeff'] == 'a']
+        counts = sorted(counts, key=lambda x: x[1], reverse=True)
+        counts = counts[:n_include]
+        out_keys = [x[0] for x in counts]
+        names = [cdl[int(c)][0] for c in out_keys]
+        print(names)
+        df = df.loc[df['label'].apply(lambda x: x in [x[0] for x in counts])]
+
+        tfile = os.path.join(traces, 'model_sft_{}.nc'.format(code))
+        trace = az.from_netcdf(tfile)
+
+        labels = [int(x) for x in list(trace.posterior['a'].labels.values)]
+        label_idx = [labels.index(c) for c in out_keys if c in labels]
+
+        alpha = trace.posterior['a'].values
+        alpha = alpha.reshape(alpha.shape[0] * alpha.shape[1], -1)
+        dct = {'a[{}]'.format(k): alpha[:, i] for i, k in zip(label_idx, out_keys)}
+
+        coeffs = trace.posterior['b'].features.values
+        arr = trace.posterior['b'].values
+        arr = arr.reshape(arr.shape[0] * arr.shape[1], arr.shape[2], -1)
+        for i, c in enumerate(coeffs):
+            for j, k in zip(label_idx, out_keys):
+                dct['b[{}, {}]'.format(c, k)] = arr[:, i, j]
+        data_df = pd.DataFrame(dct)
+        min_max_cols = [i for i in df.index if df.loc[i, 'coeff'] != 'a']
+        min_, max_ = data_df[min_max_cols].values.flatten().min(), data_df[min_max_cols].values.flatten().max()
+        fig, ax = plt.subplots(1, 5, figsize=(16, 8))
+        for i, (name, key) in enumerate(zip(names, out_keys)):
+            cols = [i for i in df.index if df.loc[i, 'label'] == key and df.loc[i, 'coeff'] != 'a']
+            data = data_df[cols]
+            sns.boxplot(data, ax=ax[i])
+            ax[i].set_xticklabels(coeffs)
+            ax[i].title.set_text(name)
+            ax[i].set_ylim([min_, max_])
+
+        plt.suptitle('{} Transition Model'.format(cdl[int(code)][0]))
+        out_name = cdl[int(code)][0].replace('/', '_')
+        sfig = os.path.join(figures, '{}_{}.png'.format(code, out_name))
+        plt.savefig(sfig)
+        print(sfig)
 
 
 if __name__ == '__main__':
@@ -94,6 +149,11 @@ if __name__ == '__main__':
         root = '/home/dgketchum/data/IrrigationGIS/expansion'
 
     npy = '/media/nvm/field_pts/fields_data/partitioned_npy/cdl/met5_ag4_fr9.npy'
-    out_ = os.path.join(root, 'figures', 'crop_transitions')
-    transition_probability(npy, out_)
+    out_ = os.path.join(root, 'figures', 'crop_transitions', 'transition_probability')
+    # transition_probability(npy, out_)
+
+    out_f = os.path.join(root, 'figures', 'crop_transitions', 'transition_models')
+    summaries_ = os.path.join(root, 'analysis', 'transition', 'summaries')
+    traces = os.path.join(root, 'analysis', 'transition', 'models')
+    transition_model(summaries_, traces, out_f)
 # ========================= EOF ====================================================================
